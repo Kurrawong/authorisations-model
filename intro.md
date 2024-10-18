bc73e01e209cdf69e2a191eec5a9334581424f66
Nicholas Car <nick@kurrawong.net>
2024-10-18 21:25:20 +1000

    add intro

---
+++ intro.md
@@ -0,0 +1,24 @@
+## Introduction 
+
+1. [Purpose](#purpose)
+2. [Basic Concepts](#basic)
+
+
+<a id="purpose"></a>
+### 1. Purpose
+
+The purpose of this model is to represent "authorisations" by which is meant:
+
+> a Rule that grants Actions to an assignee Agent over an Authorised Asset
+
+This model is just a specialisation of the [Open Data Rights Language (ODRL)](https://www.w3.org/TR/odrl-model/) model, but it was though necessary to make this model, rather than to just point to ODRL for use, for while ODRL work perfectly well for the scenarios exemplified here (see BAsic Concepts for expected use below), ODRL's own examples are all oriented elsewhere - to the use of data objects.
+
+This model aims to use names for elements and descriptions of them that are more intuitive within its target domains.
+
+
+<a id="basic"></a>
+### 2. Basic Concepts
+
+Typical use is expected to be modelling of permissions granting to people and companies for activities such as mining prospecting and the modelling of powers of government.
+
+The things granting the authorisation are Authorities: laws, policies, regulations etc.
--- scripts/make_html.py
+++ scripts/make_html.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 from pylode.profiles.ontpub import OntPub
 from textwrap import indent
+from markdown import markdown
 
 REPO_DIR = Path(__file__).parent.parent.resolve()
 
@@ -17,11 +18,45 @@ name_plus_img = f'''<h1>{name}</h1>
         <img src="img/overview.svg" width="75%"/>'''
 html = indent(re.sub(r"<h1>(.*)</h1>", name_plus_img, html), "        ")
 
+# add examples images
 html = re.sub('<h2>Classes</h2>', '''<h2>Classes</h2>
                 <img src="img/geoname.svg" width="50%"/>
                 <br />
                 <img src="img/mining-permit.svg" width="50%"/>
                 <br />''', html)
+
+# add Introduction
+html = re.sub(
+    '<div class="section" id="classes">',
+    '<div class="section" id="intro">' +
+    indent(markdown(open("intro.md").read()), "       ") +
+    '</div>' +
+    '<div class="section" id="classes">' ,
+    html
+)
+
+html = re.sub(
+    r'''<li>(\s*)<h4>(\s*)<a href="#metadata">Metadata</a>(\s*)</h4>(\s*)</li>''',
+'''<li>
+                  <h4>
+                    <a href="#metadata">Metadata</a>
+                  </h4>
+                </li>
+                <li>
+                  <h4>
+                    <a href="#intro">Introduction</a>
+                  </h4>
+                  <ul class="second">
+                    <li>
+                      <a href="#purpose">Purpose</a>
+                    </li>
+                    <li>
+                      <a href="#basic">Basic Concepts</a>
+                    </li>
+                  </ul>                  
+                </li>''',
+    html
+)
 
 # write HTML to file
 open(REPO_DIR / "model.html", "w").write(html)
